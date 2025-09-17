36afc8734ccc5b74b9cdaebe4412dbd47c31434d
Elias Huarte <elias@guander.mx>
2025-09-16 21:00:40 -0600

    added strapi cms

---
+++ src/api/nombreproducto/controllers/nombreproducto.ts
@@ -0,0 +1,7 @@
+/**
+ * nombreproducto controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::nombreproducto.nombreproducto');
---
+++ src/api/nombreproducto/services/nombreproducto.ts
@@ -0,0 +1,7 @@
+/**
+ * nombreproducto service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::nombreproducto.nombreproducto');
